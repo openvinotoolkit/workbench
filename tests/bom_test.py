98bdc9c63065b86fb9f8ef88ccf80635ac6825a8
Salnikov Igor <IgorX.Salnikov@intel.com>
2022-03-23 11:01:25 +0300

    [75756] Update bootstrap scripts (#14)

--- tests/bom_test.py
+++ tests/bom_test.py
@@ -76,7 +76,7 @@ FILE_PATTERNS_TO_SKIP = (
     r'^wb\/utils\/git_hooks_checker\.py$',
     r'^scripts\/development\/erase_db\.sql$',
     r'^scripts\/development\/check_git_remote_pre_push\.py$',
-    r'^docker\/docker-compose(\.local)?(\.bootstrap)?\.yml$'
+    r'^docker\/docker-compose(\.local)?(\.test\.bootstrap)?\.yml$'
 )
 
 FULL_NAME_PATTERNS_TO_SKIP = (
