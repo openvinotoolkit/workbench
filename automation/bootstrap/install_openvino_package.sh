e189c1550737f704c65e63c4ca0e42fe6b7c03db
Igor Salnikov <IgorX.Salnikov@intel.com>
2022-04-01 15:10:24 +0300

    Update after review

--- automation/bootstrap/install_openvino_package.sh
+++ automation/bootstrap/install_openvino_package.sh
@@ -3,7 +3,14 @@
 set -e
 
 OPENVINO_FOLDER="${HOME}/intel/openvino_2022/"
-OPENVINO_LIB_LINK="https://storage.openvinotoolkit.org/repositories/openvino/packages/2022.1/l_openvino_toolkit_runtime_ubuntu20_p_2022.1.0.643.tgz"
+OPENVINO_PACKAGE_URL="https://storage.openvinotoolkit.org/repositories/openvino/packages"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PACKAGE_TYPE='openvino_runtime_package_darwin'
+else
+  PACKAGE_TYPE='openvino_runtime_package_ubuntu'
+fi
+PACKAGE=$(grep ${PACKAGE_TYPE} ${OPENVINO_WORKBENCH_ROOT}/automation/Jenkins/openvino_version.yml | awk '{print $2}')
+OPENVINO_LIB_LINK="${OPENVINO_PACKAGE_URL}/${PACKAGE}"
 
 mkdir -p -m 777 ${OPENVINO_FOLDER}
 
